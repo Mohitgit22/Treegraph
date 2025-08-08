22aacbd573084f25f0f0b614b4b46a4810b4380a
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-08-08 12:30:50 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (27.97%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,14 +5,14 @@ public:
         int maj_el = nums[0];
 
         for(int i = 1; i < nums.size(); i++){
-           
-
-          if(nums[i] != maj_el){
-            cnt--;
              if(cnt == 0){
                 cnt =1;
                 maj_el = nums[i];
+                continue;
             }
+
+          if(nums[i] != maj_el){
+            cnt--;
           }else
           cnt++;
         }
