958dd50977b304d37a7b0bd141788af181ebb40b
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-08-09 12:26:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.82%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int findpvt(vector<int>& nums){
-        int l = 0;
-        int r = nums.size()-1;
-      
-        int mid = -1;
-        while( l < r){
-            mid = l + (r-l)/2;
-            if(nums[mid] > nums[r]) 
-            l = mid + 1;
-            else
-            r = mid;
+    int findpvt(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n-1;
+
+        while(l < r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] > nums[r]){
+                l = mid + 1;
+            }else
+             r = mid;
         }
         return l;
     }
 
-    int search(int l, int r, int t, vector<int>& nums) {
+    int search(vector<int>& nums, int start, int end, int t){
+        int l =start, r = end;
+
         while(l <= r){
             int mid = l + (r-l)/2;
-
             if(nums[mid] == t) return mid;
             else if(nums[mid] > t)
             r = mid-1;
@@ -28,14 +28,11 @@ public:
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int pivot = findpvt(nums);
-        cout<<pivot<<"is"<<endl;
+        int pvt = findpvt(nums);
         int n = nums.size();
 
-        int el = search(0, pivot-1, target, nums);
-
-        int el2 = search(pivot, n-1, target, nums);
-        
-        return max(el, el2);
+        int el1 = search(nums, 0, pvt-1, target);
+        int el2 = search(nums, pvt, n-1, target);
+        return max(el1, el2);
     }
 };
