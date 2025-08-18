22ede8d784dceda8c61c15650a027490a7d611f2
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-08-18 20:45:58 +0530

    Time: 2 ms (34.21%) | Memory: 16.8 MB (75.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,51 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> que;
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 2){
-                    que.push({i, j});
-                }
-            }
-        }
        
-        int time = 0;
-        int dx[4] = { 0, 0, -1, 1};
-        int dy[4] = {-1, 1, 0, 0};
-        while(!que.empty()){
-            int sz = que.size();
-            bool rottedThisMinute = false;
+       
+       queue<pair<pair<int, int>, int>> que;
+       int m = grid.size();
+       int n = grid[0].size();
 
-            while(sz > 0){
-               int x = que.front().first;
-               int y = que.front().second;
-               que.pop();
-               sz--;
-               for(int i = 0; i < 4; i++){
-                int newx = x + dx[i];
-                int newy = y + dy[i];
+    //    bool flag = false;
 
-                if(newx >= 0 and newx < m and newy >= 0 and newy < n and grid[newx][newy] == 1){
-                     grid[newx][newy] = 2;
-                     que.push({newx, newy});
-                     rottedThisMinute = true;
-                }
-              }  
-            }
-            if(rottedThisMinute) time++;
+       for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++){
+            if(grid[i][j] == 2)
+            que.push({{i, j}, 0});
         }
+       }
 
-         for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1){
-                    return -1;
-                }
+       int dx[4] = {-1, 1, 0, 0};
+       int dy[4] = {0, 0, 1, -1};
+       int t = 0;
+       while(!que.empty()){
+        auto front = que.front();
+        que.pop();
+
+        pair<int , int> coord = front.first;
+         t = max(t, front.second);
+        int x = coord.first;
+        int y = coord.second;
+
+        for(int i = 0; i < 4; i++){
+            int newx = x + dx[i];
+            int newy = y + dy[i];
+
+            if(newx >= 0 and newy >= 0 and newx < m and newy < n and grid[newx][newy] == 1){
+                grid[newx][newy] = 2;
+                que.push({{newx, newy}, t+1});
             }
         }
-      return time;
+       }
+
+       for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++){
+            if(grid[i][j] == 1)
+            return -1;
+        }
+       }
+       return t;
     }
 };
