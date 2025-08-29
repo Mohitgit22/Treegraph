10e3f77181d937a1bda30cf00ca8de728dfd4957
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-08-29 12:11:05 +0530

    Time: 2 ms (77.30%) | Memory: 7.8 MB (63.99%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -21,19 +21,36 @@ public:
     //     }
 
 
+    //bottom up
+        // int fib(int n) {
+        // if(n <= 1) return n;
 
-        int fib(int n) {
-        if(n <= 1) return n;
-
-        vector<int>dp(n+1);
+        // vector<int>dp(n+1);
 
         //base case
-        dp[0] = 0;
-        dp[1] = 1;
+        // dp[0] = 0;
+    //     dp[1] = 1;
+
+    //     for(int i = 2; i < n+1; i++){
+    //         dp[i] = dp[i-1] + dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+
+       //space optimized bottom up
+       int fib(int n) {
+        if(n <= 1) return n;
+
+        //base case
+         int prev2 = 0;
+         int prev1 = 1;
+         int ans = 0;
 
         for(int i = 2; i < n+1; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            ans = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = ans;
         }
-        return dp[n];
+        return ans;
     }
 };
