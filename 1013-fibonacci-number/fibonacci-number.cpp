3df48d9fff7a44ae7251f449a1261b88f81d7bf5
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-08-29 12:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (20.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,21 +7,33 @@ public:
     //     return fib(n-1) + fib(n-2);
     // }
         
-       int  top_down(int n, vector<int>& dp){
-         if(n <= 1){
-            dp[n] = n;
-            return dp[n];
-         }
+    //    int  top_down(int n, vector<int>& dp){
+    //      if(n <= 1){
+    //         dp[n] = n;
+    //         return dp[n];
+    //      }
 
-         if(dp[n] != -1) return dp[n];
+    //      if(dp[n] != -1) return dp[n];
 
-         dp[n] = top_down(n-1, dp) + top_down(n-2, dp);
+    //      dp[n] = top_down(n-1, dp) + top_down(n-2, dp);
           
-         return dp[n];
-        }
+    //      return dp[n];
+    //     }
+
+
 
         int fib(int n) {
-        vector<int>dp(n+1, -1);
-        return top_down(n, dp);
+        if(n <= 1) return n;
+
+        vector<int>dp(n+1);
+
+        //base case
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i < n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
