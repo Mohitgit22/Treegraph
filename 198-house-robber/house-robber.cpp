be39ebb7b9027506621ec4a27df85d26dce2e721
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-08-30 12:48:39 +0530

    Time: 3 ms (5.78%) | Memory: 10.8 MB (22.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,21 +2,32 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return recur(n-1, nums, dp);
-    }
+        vector<int> dp(n+1,  -1);
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-    int recur(int n, vector<int>& nums, vector<int>& dp){
-        if(n < 0){
-        return 0;
+        for(int i =2; i < n+1; i++) {
+           int pick = nums[i-1] + dp[i-2];
+           int notpick = dp[i-1];
+           dp[i] = max(pick, notpick);
         }
-       
-        if(dp[n] != -1) return dp[n];
-       
-        int pick = nums[n] + recur(n-2, nums, dp);
-        int notpick = recur(n-1, nums, dp);
-
-        dp[n] = max(pick, notpick);
         return dp[n];
     }
+
+    // int recur(int n, vector<int>& nums, vector<int>& dp){
+    //     if(n < 0){
+    //     return 0;
+    //     }
+
+    // if(n==0)
+    // return nums[n];
+       
+    //     if(dp[n] != -1) return dp[n];
+       
+    //     int pick = nums[n] + recur(n-2, nums, dp);
+    //     int notpick = recur(n-1, nums, dp);
+
+    //     dp[n] = max(pick, notpick);
+    //     return dp[n];
+    // }
 };
