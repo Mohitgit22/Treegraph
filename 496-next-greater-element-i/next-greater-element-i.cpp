aef19ff2796338054faf7178f684bc9c04d9aaf7
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-05 22:57:27 +0530

    Time: 3 ms (42.12%) | Memory: 13.1 MB (15.34%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void ngr(vector<int>& nums2, map<int, int>& mp){
-        int n = nums2.size();
+    void ngr(vector<int>& nums2, int n, map<int , int> &mp){
         stack<int> st;
         for(int i = n-1; i>= 0; i--){
-         
-        while(!st.empty() and st.top() < nums2[i]) st.pop();
+          while(!st.empty() and st.top() <= nums2[i]){
+            st.pop();
+          }
 
-        mp[nums2[i]] = st.empty() ? -1 : st.top();
+          int x = st.empty() ? -1 : st.top();
+          mp[nums2[i]] = x;
 
-        st.push(nums2[i]);
+          st.push(nums2[i]);
         }
     }
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        map<int, int> mp;
-        ngr(nums2, mp);
 
-        for(auto it : nums1){
-            if(mp.find(it)!= mp.end()){
-                res.push_back(mp[it]);
-            }else
-            res.push_back(-1);
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        map<int, int> mp;
+        ngr(nums2, n, mp);
+       
+        vector<int> ans;
+        for(int it : nums1){
+            ans.push_back(mp[it]);
         }
-        return res;
+        return ans;
     }
 };
