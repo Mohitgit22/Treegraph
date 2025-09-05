0bb476cbe9de44bd78c112ab0d287e2b4c169b1c
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-06 00:05:56 +0530

    Time: 12 ms (83.87%) | Memory: 51.6 MB (5.39%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,52 +1,57 @@
 class Solution {
 public:
-   const int MOD = 1e9 + 7;
+  const int MOD = 1e9 + 7;
+  vector<int>  NSR(vector<int>& arr, int n){
+        stack<int> st;
+       vector<int> res(n);
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() and arr[st.top()] >= arr[i]){
+                st.pop();
+            }
 
-    void nsr_idx(vector<int>& arr, vector<int>& nsr) {
-    stack<int> st;
-    int n = arr.size();
-    for(int i = n-1; i >= 0; i--){
-
-        while(!st.empty() and arr[st.top()] >= arr[i]) st.pop();
-
-        int el = st.empty() ? n : st.top();
-
-        nsr.push_back(el);
-
-        st.push(i);
-    }
-    reverse(nsr.begin(), nsr.end());
+            int x = st.empty() ? n : st.top();
+            res[i] = x;
+            st.push(i);
+        }
+        return res;
     }
 
-    void nsl_idx(vector<int>& arr, vector<int>& nsl) {
-    stack<int> st;
-    int n = arr.size();
-    for(int i = 0; i < n; i++){
+    vector<int> NSL(vector<int>& arr, int n){
+        stack<int> st;
+        vector<int> res(n);
+        for(int i = 0; i < n; i++){
+            while(!st.empty() and arr[st.top()] > arr[i]){
+                st.pop();
+            }
 
-        while(!st.empty() and arr[st.top()] > arr[i]) st.pop();
+            int x = st.empty() ? -1 : st.top();
 
-        int el = st.empty() ? -1 : st.top();
-
-        nsl.push_back(el);
-
-        st.push(i);
-    }
+            res[i] = x;
+            st.push(i);
+        }
+        return res;
     }
 
     int sumSubarrayMins(vector<int>& arr) {
-        vector<int>nsr, nsl;
-        nsr_idx(arr, nsr);
-        nsl_idx(arr, nsl);
+        int n = arr.size();
+
+        vector<int>nsr = NSR(arr, n);
+        vector<int> nsl = NSL(arr, n);
+
+         vector<int> left(n), right(n), contribution(n);
+        for(int i = 0; i < n; i++){
+         left[i]  = i - nsl[i];
+         right[i] = nsr[i] - i;
+         contribution[i] = ((((1LL * left[i] * right[i]) % MOD) * arr[i]) % MOD);
+        }
+        
 
         int sum = 0;
-
-        for(int i = 0; i < arr.size(); i++){
-            int left = i - nsl[i];
-            int right = nsr[i] - i;
-            long long contribution = (1ll*arr[i] * ((left * right) % MOD)) % MOD;
-
-            sum = (sum + contribution) % MOD;
+        for(int it: contribution){
+            sum += it;
+            sum = sum % MOD;
         }
+        sum = sum % MOD;
         return sum;
     }
 };
