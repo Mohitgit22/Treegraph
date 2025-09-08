b2cb2069e9e94df763cf62005867c272adafac4b
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-09 00:20:02 +0530

    Time: 28 ms (26.09%) | Memory: 38.1 MB (49.84%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<pair<int, int>, int>> que;
-        
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        int m = mat.size();
+        int n = mat[0].size();
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        vector<vector<int>> vec(m, vector<int>(n, 0));
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        queue<pair<int, pair<int, int>>> que;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(mat[i][j] == 0){
-                    que.push({{i, j}, 0});
                     vis[i][j] = true;
+                    que.push({0,{i,j}});
                 }
             }
         }
 
-       int dx[4] = {0, 0, -1, 1};
-       int dy[4] = {-1, 1, 0, 0};
+        int dx[4] = {-1,1, 0, 0};
+        int dy[4] = {0, 0, -1, 1};
 
         while(!que.empty()){
-            int x = que.front().first.first;
-            int y = que.front().first.second;
-            int dist = que.front().second;
+            int d = que.front().first;
+            int x = que.front().second.first;
+            int y = que.front().second.second;
             que.pop();
-            
-            for(int i = 0; i < 4; i++){
-                int newx = x + dx[i];
-                int newy = y + dy[i];
 
-                if(newx >= 0 and newx < n and newy >= 0 and newy < m and mat[newx][newy] == 1 and !vis[newx][newy]){
-                    vis[newx][newy] = true;
-                    mat[newx][newy] = dist+1;
-                    que.push({{newx, newy}, dist + 1});
-                }
+           for(int i = 0; i < 4; i++){
+            int newx = x + dx[i];
+            int newy = y + dy[i];
+
+            if(newx >= 0 and newx< m and newy >= 0 and newy < n and mat[newx][newy] == 1 and !vis[newx][newy]){
+                vis[newx][newy] = true;
+                vec[newx][newy] = d+1;
+                que.push({d+1, {newx, newy}});
             }
+           }
         }
-    return mat;
-
+        return vec;
     }
 };
