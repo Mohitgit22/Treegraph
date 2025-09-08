069f51e79957907231e9b9d1edb1723d93b7960b
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-08 21:43:54 +0530

    Time: 3 ms (42.12%) | Memory: 20.6 MB (40.22%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-
-  void bfs(vector<vector<int>> &adj,int u,  queue<int> &que, vector<bool> &vis, int &cnt) {
-      que.push(u);
-
-      while(!que.empty()){
-        int u = que.front();
-        que.pop();
-        vis[u] = true;
-
-        for(int &v : adj[u]){
-            if(!vis[v]){
-                vis[v] = true;
-                que.push(v);
-            }
-        }
-      }
-    }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-
         vector<vector<int>> adj(n);
 
-        for(int i = 0; i < n; i++ ){
-            for(int j = 0; j < isConnected[i].size(); j++){
-                if(isConnected[i][j] == 1 and i != j){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j and isConnected[i][j] == 1){
                     adj[i].push_back(j);
                 }
             }
         }
-       
-        queue<int> que;
+
         vector<bool> vis(n, false);
         int cnt = 0;
-
-        for(int i = 0; i < n; i++){
+        for(int i =0; i< n; i++){
             if(!vis[i]){
                 cnt++;
-                bfs(adj, i, que, vis, cnt);
+                bfs(adj, i, vis);
             }
         }
         return cnt;
-    }  
+    }
+
+    void bfs(vector<vector<int>>& adj, int u, vector<bool>& vis){
+        queue<int> que;
+
+        que.push(u);
+        vis[u] = true;
+
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+
+            for(int &v: adj[u]){
+                if(!vis[v]){
+                    vis[v] = true;
+                    que.push(v);
+                }
+            }
+        }
+    }
 };
