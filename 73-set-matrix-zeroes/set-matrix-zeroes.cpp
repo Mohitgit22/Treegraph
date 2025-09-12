5cecea82f12cc5f6952b09b611a38822180725fd
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-12 23:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.48%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int> rows;
-        set<int> columns;
-       
         int m = matrix.size();
         int n = matrix[0].size();
+        bool flag = false;
+
         for(int i = 0; i < m; i++){
-            for(int j = 0; j< n; j++){
+            if(matrix[i][0] == 0)
+                  flag = true;
+            for(int j = 1; j< n; j++){
                 if(matrix[i][j] == 0){
-                    rows.insert(i);
-                    columns.insert(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j< n; j++){
-                if(rows.find(i) != rows.end() || columns.find(j) != columns.end()){
-                    matrix[i][j] = 0;
-                }
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j > 0; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                   matrix[i][j] = 0;
             }
+            if(flag)
+            matrix[i][0] = 0;
         }
+
+        
     }
 };
