c6f17143f0c3c02ddbc4d73aaa7886ee0479b37d
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-14 18:28:16 +0530

    Time: 59 ms (15.35%) | Memory: 53.9 MB (8.49%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount + 1, -1));
+        int res = recur(coins, n, amount, dp);
 
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
-        int res =  recur(coins, n-1, amount, dp);
-
-        if(res == 1e9) return -1;
-        return res;
+        return res == 1e9 ? -1 : res;
     }
-
-    int recur(vector<int>& coins, int ind, int amount, vector<vector<int>>& dp){
-       if(ind == 0){
-        if(amount % coins[ind] == 0)
-        return amount/coins[ind];
-        else
-        return 1e9;
-       }
     
-      if(dp[ind][amount] != -1) return dp[ind][amount];
+    int recur(vector<int>& coins, int ind, int amount, vector<vector<int>>& dp) {
+        if(amount == 0) return 0;
+        if(ind == 0) return 1e9;
 
-       int pick = 1e9;
-       if(coins[ind] <= amount){
-        pick = 1 + recur(coins, ind, amount - coins[ind], dp);
-       }
-       int nopick = recur(coins, ind-1, amount, dp);
+       if(dp[ind][amount] != -1) return dp[ind][amount];
+
+        int notpick = recur(coins, ind-1, amount, dp);
         
-       dp[ind][amount] = min(pick, nopick);
-       return dp[ind][amount];
+        int pick = 1e9;
+        if(coins[ind-1] <= amount){
+            pick = min(pick, 1 + recur(coins, ind, amount-coins[ind-1], dp));
+        }
+        dp[ind][amount] = min(pick, notpick);
+        return dp[ind][amount];
     }
 };
