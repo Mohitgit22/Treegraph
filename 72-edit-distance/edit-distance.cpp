eca27dce0734bdc432b949352cb43c232bbe6978
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-15 20:17:07 +0530

    Time: 11 ms (30.00%) | Memory: 13.2 MB (48.55%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,26 +4,26 @@ public:
         int n1 = word1.length();
         int n2 = word2.length();
          
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1)); 
-        return recur(word1, word2, n1, n2, dp);
-    }
+         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
 
-    int recur(string s1, string s2, int ind1, int ind2, vector<vector<int>>& dp){
-        if(ind1 == 0) return ind2 ;  // If s1 is exhausted, insert remaining s2 characters
-        if(ind2 == 0) return ind1 ;  // If s2 is exhausted, delete remaining s1 characters
-       
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+         for(int i = 0;i < n1+ 1; i++){
+            for(int j = 0; j < n2 +1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = max(i, j);
+                }
+            }
+         }
 
-        int mini1, mini2;
-        if(s1[ind1-1] == s2[ind2-1]){
-            dp[ind1][ind2] =  recur(s1, s2, ind1-1, ind2-1, dp);
-        }
-        else{
-            int insertOp = 1 + recur(s1, s2, ind1, ind2 - 1, dp);   // Insert character
-            int deleteOp = 1 + recur(s1, s2, ind1 - 1, ind2, dp);   // Delete character
-            int replaceOp = 1 + recur(s1, s2, ind1 - 1, ind2 - 1, dp); // Replace character
-            dp[ind1][ind2] =  min({insertOp, deleteOp, replaceOp});
-        }
-        return dp[ind1][ind2];
+         for(int i = 1; i < n1 +1; i++){
+            for(int j = 1; j < n2 + 1; j++){
+                if(word1[i-1] == word2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] =1 +  min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+                }
+            }
+         }
+
+        return dp[n1][n2];
     }
 };
