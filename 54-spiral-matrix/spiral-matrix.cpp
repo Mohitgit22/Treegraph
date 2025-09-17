2caba6f5d6c7d50c68c7949c6b1cb9dbfe38785c
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-17 19:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.57%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,49 +1,43 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        
         int m = matrix.size();
         int n = matrix[0].size();
 
-        int top = 0, left = 0, right = n-1, down = m-1;
-        vector<int> ans;
+        int top = 0, down = m-1, left = 0, right = n-1;
         int dir = 0;
+        vector<int> ans;
 
         while(top <= down and left <= right){
-       
-          if(dir == 0){
-            for(int i = left; i <= right; i++){
-                ans.push_back(matrix[top][i]);
+            if(dir == 0){
+                for(int i = left; i <= right; i++){
+                    ans.push_back(matrix[top][i]);
+                }
+                top++;
             }
-            top++;
-          }
-
-          if(dir == 1){
-            for(int i = top; i <= down; i++){
-                ans.push_back(matrix[i][right]);
+            
+            if(dir == 1){
+                for(int i = top; i <= down; i++){
+                    ans.push_back(matrix[i][right]);
+                }
+                right--;
             }
-            right--;
-          }
-
-          if(dir == 2){
-             for(int i = right; i >= left; i--){
-                ans.push_back(matrix[down][i]);
+            if(dir == 2){
+                for(int i = right; i >= left; i--){
+                    ans.push_back(matrix[down][i]);
+                }
+                down--;
             }
-            down--;
-          }
-
-          if(dir == 3){
-             for(int i = down; i >= top; i--){
-                ans.push_back(matrix[i][left]);
+            if(dir == 3){
+                for(int i = down; i >= top; i--){
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
             }
-            left++;
-          }
 
           dir++;
-
           if(dir == 4)
-           dir = 0;
-
+          dir = 0;
         }
         return ans;
     }
