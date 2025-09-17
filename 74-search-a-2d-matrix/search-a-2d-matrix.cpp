d620ce8fa935be2500b7f6bd0b68c0a873b54708
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-17 20:52:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (7.50%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    bool bs(vector<int> v, int t){
-        int l = 0, r = v.size()-1;
+   bool bs(vector<int>& vec, int t){
+    int l = 0, r = vec.size()-1;
+     while(l <= r){
+        int mid = l + (r-l)/2;
+        if(vec[mid] == t) 
+        return true;
+        else if(vec[mid] > t) 
+        r = mid-1;
+        else
+        l = mid+1;
+     }
+     return false;
+   }
 
-        while(l <= r){
-            int mid = l + (r-l)/2;
-            if(v[mid] == t) return true;
-            else if(v[mid] > t)
-            r = mid-1;
-            else
-            l = mid+1;
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        int n = matrix.size();
         vector<int> vec;
-        int m = matrix.size();
         
-        for(int i = 0; i < m; i++){
-            vec.push_back(matrix[i][0]);
+        if(n == 0 || matrix[0].size() == 0) return false;
+
+
+        for(int i = 0; i < n; i++){
+          vec.push_back(matrix[i][0]);
         }
 
         auto it = upper_bound(vec.begin(), vec.end(), target);
-        int row = it - vec.begin() -1;
-        // cout<<row;
-        if(row < 0) return false;
+        int idx = it - vec.begin() - 1;
+     
+        // cout<<"idx is : "<<idx;
+        if(idx < 0) return false;
 
-        return bs(matrix[row], target);
+        vec = matrix[idx];
+        return bs(vec, target);
     }
 };
