1f75dfbead91260cd9e0682a58cf136bc72d32c1
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-09-19 23:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int top_down(int n, vector<int>& dp) {
-        if( n <= 2){
+    int recur(int n, vector<int>& dp){
+          if(n <= 2){
             dp[n] = n;
             return dp[n];
-        }
+          }
 
-        if(dp[n] != -1)
-        return dp[n];
+          if(dp[n] != -1) return dp[n];
 
-        dp[n] = top_down(n-1, dp) + top_down(n-2, dp);
-        return dp[n];
+          dp[n] =  recur(n-1, dp) + recur(n-2, dp);
+          return dp[n];
     }
-    
     int climbStairs(int n) {
+         if(n == 1) return 1;
+         
         vector<int> dp(n+1, -1);
-        return top_down(n, dp);
+        return recur(n, dp);
     }
 };
